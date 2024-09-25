78fc91f4f2aadd5193efaf758092277adfc26cce
jeongbaebang <jeongbaebang_dev@naver.com>
2024-09-25 18:32:06 +0900

    📝 Update: 함수 스타일 변경

--- src/features/redux/Provider.tsx
+++ src/features/redux/Provider.tsx
@@ -3,6 +3,8 @@ import { Provider } from "react-redux";
 import store from "./store";
 import { ReduxProviderProps } from "./type";
 
-export default function ReduxProvider({ children }: ReduxProviderProps) {
+const ReduxProvider = ({ children }: ReduxProviderProps) => {
   return <Provider store={store}>{children}</Provider>;
-}
+};
+
+export default ReduxProvider;
