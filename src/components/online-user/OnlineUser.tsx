31dc34b0a887ffe512e382c159d921432be3596c
LimJaeHyun <guswogus81@gmail.com>
2024-09-30 11:53:57 +0900

    📝 Update: 사용자 목록 기능 구현
    온라인 / 오프라인 상태에 따라 유저 리스트를 출력

--- src/components/online-user/OnlineUser.tsx
+++ src/components/online-user/OnlineUser.tsx
@@ -1,31 +1,59 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import onlineUserIcon from "../../assets/icons/onlineUserIcon.png";
+import userIcon from "../../assets/icons/userIcon.png";
+import onlineIcon from "../../assets/icons/onlineIcon.png";
+import offlineIcon from "../../assets/icons/offlineIcon.png";
+import { useNavigate } from "react-router-dom";
 
+// 사용자 목록 가장 큰 테두리
 const OnlineUserContainer = styled.div`
   position: absolute;
   top: 150px;
   right: 30px;
   width: 250px;
   max-height: 500px;
-  border: 1px solid #ccc; // 보더라인 유무 - 추후 결정
+  /* border: 1px solid #ccc; // 보더라인 유무 - 추후 결정 */
   display: flex;
   flex-direction: column;
   /* overflow-x: hidden;
   overflow-y: scroll; */
 `;
 
+// "접속 중인 유저" 상단 타이틀
 const OnlineUserTitle = styled.div`
+  img {
+    width: 10px;
+    margin-right: 10px;
+    pointer-events: none; // 클릭했을때 브라우저 오류를 예방하기 위한 코드
+  }
   width: 250px;
   max-height: 500px;
+  align-items: center;
+  pointer-events: none; // 클릭했을때 브라우저 오류를 예방하기 위한 코드
 `;
 
+// "접속 중인 유저" 유저들 리스트
 const OnlineUserList = styled.div`
   width: 250px;
   max-height: 500px;
+  margin-top: 20px;
+  margin-left: 10px;
 `;
 
-const OnlineUserBox = styled.div``;
+// 개별 유저 리스트 -> 프로필 사진(온라인/오프라인) : {user.fullName}
+const OnlineUserBox = styled.div`
+  img {
+    width: 30px;
+    margin-left: 10px;
+    margin-right: 10px;
+  }
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
 
 interface OnlineUserI {
   isOnline: string;
@@ -34,6 +62,7 @@ interface OnlineUserI {
 }
 
 const OnlineUser = () => {
+  const navigate = useNavigate(); // 내비게이터
   const [onlineUserList, setOnlineUserList] = useState<OnlineUserI[]>([]);
   // 접속중인 유저 데이터를 API를 통해 가져온다. : fetchOnlineUser() 함수 생성
   useEffect(() => {
@@ -51,18 +80,25 @@ const OnlineUser = () => {
     fetchOnlineUser();
   }, []);
 
-  // API로 가져온 데이터를 출력한다.
-  // 만약 온라인이라면 "onlineUserIcon + user.fullName"
-  // 만약 오프라인이라면 "userIcon + user.fullName"
-  // 클릭 시 해당 유저의 프로필로 이동 : /profile/{user._id}
-
   return (
     <OnlineUserContainer>
-      <OnlineUserTitle>접속 중인 유저</OnlineUserTitle>
+      <OnlineUserTitle>
+        {onlineUserList.length > 0 ? <img src={onlineIcon} /> : <img src={offlineIcon} />}
+        접속 중인 유저
+      </OnlineUserTitle>
       <OnlineUserList>
-        <OnlineUserBox>프로필사진 / 유저이름1</OnlineUserBox>
-        <OnlineUserBox>프로필사진 / 유저이름2</OnlineUserBox>
-        <OnlineUserBox>프로필사진 / 유저이름3</OnlineUserBox>
+        {onlineUserList.map((user) => {
+          return (
+            <OnlineUserBox
+              key={user._id}
+              onClick={() => {
+                navigate(`/profile/${user._id}`);
+              }}>
+              <img src={onlineUserIcon} />
+              <div>{user.fullName}</div>
+            </OnlineUserBox>
+          );
+        })}
       </OnlineUserList>
     </OnlineUserContainer>
   );
