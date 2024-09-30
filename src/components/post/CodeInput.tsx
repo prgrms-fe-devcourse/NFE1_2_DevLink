acad40ac33682b541924e62609a9a9a4dbff50fb
jeongbaebang <jeongbaebang_dev@naver.com>
2024-09-30 17:13:58 +0900

    🔨 Add: 코드 입력 컴포넌트 & 코드 포맷 로직 추가

--- src/components/post/CodeInput.tsx
+++ src/components/post/CodeInput.tsx
@@ -1,24 +1,40 @@
-import { Typography } from "antd";
+import { useRef } from "react";
 import styled from "styled-components";
-import { Editor, EditorProps } from "@monaco-editor/react";
+import { Button, Flex, Tooltip, Typography } from "antd";
 import { TitleProps } from "antd/es/typography/Title";
+import { FormatPainterOutlined } from "@ant-design/icons";
+import { editor } from "monaco-editor";
+import { Editor, EditorProps, Monaco } from "@monaco-editor/react";
+
+import { formatCode } from "../../utils/formatCode";
 
 interface CodeInputProps {
   onChange: (code?: string) => void;
 }
 
 const config = {
-  label: "CODE SANDBOX",
-  text: 5 as TitleProps["level"],
+  text: {
+    size: 5 as TitleProps["level"],
+    tooltip: "코드 포맷팅",
+    format: "Format",
+  },
   code: {
-    lineNumbers: "on",
-    scrollBeyondLastLine: false,
-    automaticLayout: true,
-    minimap: {
-      enabled: false,
+    language: "typescript",
+    theme: "vs-dark",
+    options: {
+      lineNumbers: "on",
+      scrollBeyondLastLine: false,
+      automaticLayout: true,
+      minimap: {
+        enabled: false,
+      },
+      fontSize: 14,
     },
-    fontSize: 14,
-  } as EditorProps["options"],
+    path: "Component.tsx",
+    defaultValue:
+      "const Component = () => {\n  return (\n    <div style={styles}>\n      <h1>Hello World</h1>\n    </div>\n  );\n};\n\nconst styles = {};\n",
+    loading: <Typography.Text type="warning">에디터 준비중...</Typography.Text>,
+  } as EditorProps,
 };
 
 /**
@@ -28,20 +44,57 @@ const config = {
  * 사용자가 입력한 본문 내용은 `onChange` 핸들러를 통해 상위 컴포넌트로 전달됩니다.
  */
 const CodeInput: React.FC<CodeInputProps> = ({ onChange }) => {
+  const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+  const onMountHandler = handleEditorWillMount((editor) => {
+    editorRef.current = editor;
+  });
+  const onFormatHandler = async () => {
+    if (!editorRef.current) return;
+
+    const unformattedCode = editorRef.current.getValue();
+    const formattedCode = await formatCode(unformattedCode);
+
+    if (formattedCode.code !== null) {
+      editorRef.current.setValue(formattedCode.code);
+    }
+  };
+
   return (
     <Container>
-      <Typography.Title level={config.text}>{config.label}</Typography.Title>
+      <Flex justify="space-between" align="end">
+        <Typography.Title level={config.text.size}>{config.code.path}</Typography.Title>
+        <Tooltip title={config.text.tooltip}>
+          <Button type="text" icon={<FormatPainterOutlined />} onClick={onFormatHandler}>
+            {config.text.format}
+          </Button>
+        </Tooltip>
+      </Flex>
+
       <CodeSandBox>
-        <Editor
-          onChange={onChange}
-          language="typescript"
-          theme="vs-dark"
-          value={"function helloWorld() {\n  console.log('Hello, World!');\n}"}
-          options={config.code}
-        />
+        <Editor onChange={onChange} onMount={onMountHandler} {...config.code} />
       </CodeSandBox>
     </Container>
   );
+};
+
+const handleEditorWillMount = (callback: (arg: editor.IStandaloneCodeEditor) => void) => {
+  return (editor: editor.IStandaloneCodeEditor, monaco: Monaco) => {
+    monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+      noSemanticValidation: true,
+      noSyntaxValidation: false,
+    });
+
+    monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
+      jsx: monaco.languages.typescript.JsxEmit.React,
+      jsxFactory: "React.createElement",
+      reactNamespace: "React",
+      allowJs: true,
+      skipLibCheck: true,
+      esModuleInterop: true,
+    });
+
+    callback(editor);
+  };
 };
 
 const Container = styled.div`
@@ -53,7 +106,7 @@ const Container = styled.div`
 const CodeSandBox = styled.section`
   width: 100%;
   height: 100%;
-  background: papayawhip;
+  background: #1e1e1e;
 `;
 
 export default CodeInput;
