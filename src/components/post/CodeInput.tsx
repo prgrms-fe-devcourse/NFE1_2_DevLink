a7da1de36c36b684dc1a7b5aafbca5c9321d5801
jeongbaebang <jeongbaebang_dev@naver.com>
2024-09-30 17:31:11 +0900

    📝 Update: CodeInput 코드 개선
    -  개방-폐쇄 원칙 (Open/Closed Principle)에 따라 config 구성을 외부에서 주입받도록 구성

--- src/components/post/CodeInput.tsx
+++ src/components/post/CodeInput.tsx
@@ -8,13 +8,23 @@ import { Editor, EditorProps, Monaco } from "@monaco-editor/react";
 
 import { formatCode } from "../../utils/formatCode";
 
+type Config = {
+  text: {
+    fontLevel: TitleProps["level"];
+    tooltip: string;
+    format: string;
+  };
+  code: EditorProps;
+};
+
 interface CodeInputProps {
   onChange: (code?: string) => void;
+  config?: Config;
 }
 
-const config = {
+const defaultConfig = {
   text: {
-    size: 5 as TitleProps["level"],
+    fontLevel: 5 as TitleProps["level"],
     tooltip: "코드 포맷팅",
     format: "Format",
   },
@@ -43,11 +53,13 @@ const config = {
  * 해당 컴포넌트는 포스트의 코드를 입력하기 위한 입력 폼입니다.
  * 사용자가 입력한 본문 내용은 `onChange` 핸들러를 통해 상위 컴포넌트로 전달됩니다.
  */
-const CodeInput: React.FC<CodeInputProps> = ({ onChange }) => {
+const CodeInput: React.FC<CodeInputProps> = ({ onChange, config = defaultConfig }) => {
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
+
   const onMountHandler = handleEditorWillMount((editor) => {
     editorRef.current = editor;
   });
+
   const onFormatHandler = async () => {
     if (!editorRef.current) return;
 
@@ -62,7 +74,7 @@ const CodeInput: React.FC<CodeInputProps> = ({ onChange }) => {
   return (
     <Container>
       <Flex justify="space-between" align="end">
-        <Typography.Title level={config.text.size}>{config.code.path}</Typography.Title>
+        <Typography.Title level={config.text.fontLevel}>{config.code.path}</Typography.Title>
         <Tooltip title={config.text.tooltip}>
           <Button type="text" icon={<FormatPainterOutlined />} onClick={onFormatHandler}>
             {config.text.format}
