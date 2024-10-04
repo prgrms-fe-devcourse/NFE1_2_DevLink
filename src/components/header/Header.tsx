5010f5731463759d53588faadb31a20103aaf609
LimJaeHyun <guswogus81@gmail.com>
2024-10-04 12:36:22 +0900

    📝 Update: 상단 헤더
    - 디폴트 헤더 로그아웃 상태로 변경
    - localStorage의 userToken이 있다면 로그인 상태의 헤더 출력
    - 유저 fullName 클릭 시 마이페이지로 이동
    - 로그아웃 누르면 localStorage 삭제

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import devlinkLogo from "../../assets/images/devlinkLogo.png";
 import signoutIcon from "../../assets/icons/signoutIcon.png";
@@ -10,8 +10,8 @@ import styled from "styled-components";
 const HeaderBackground = styled.div`
   width: 100%;
   height: 60px;
-  padding: 0px;
-  margin: 0px;
+  padding: 0;
+  margin: 0;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -62,55 +62,96 @@ const HeaderButtons = styled.div`
   }
 `;
 
-const Header = () => {
-  // 더미 데이터 : 삭제 예정 (테스트 전용 변수)
-  const [islogin, setIslogin] = useState(true);
-  const [username, setUsername] = useState("유저이름(수정필요)"); // user.fullName으로 변경 요망
-  const [localStorageToken, SetLocalStorageToken] = useState(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY2ZmEwODNlZGNkM2Y1NzM1M2EyYTExZiIsImVtYWlsIjoiYWFhQG5hdmVyLmNvbSJ9LCJpYXQiOjE3Mjc4MzcwMzF9.adV1I6cBqAIknVEyb08hDvSasDVcEPPdfp84qr4vNJ0"
-  );
-  // ‼️‼️삭제 예정‼️‼️  ‼️‼️삭제 예정‼️‼️  ‼️‼️삭제 예정‼️‼️  ‼️‼️삭제 예정‼️‼️  ‼️‼️삭제 예정‼️‼️
+// 유저 인터페이스 정의
+interface User {
+  _id: string;
+  fullName: string;
+  // 필요한 경우 다른 필드 추가
+}
+
+const Header: React.FC = () => {
+  const [isLogin, setIsLogin] = useState<boolean>(false);
+  const [userToken, setUserToken] = useState<string | null>(null);
 
   // 상단 내비게이션 : 클릭 시 해당 주소로 이동
   const navigate = useNavigate();
 
-  // 로그아웃 버튼 클릭 시 로그아웃 API 요청 - 수정 중
+  // 로그인 확인 함수
+  useEffect(() => {
+    const checkLogin = () => {
+      const token = window.localStorage.getItem("userToken");
+      setIsLogin(!!token);
+      setUserToken(token);
+    };
+    checkLogin();
+  }, []);
+
+  // 로그인 유저의 _id를 가져오는 함수
+  const [loginUser, setLoginUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getUser = async () => {
+      if (userToken) {
+        try {
+          const response = await fetch(
+            "https://kdt.frontend.5th.programmers.co.kr:5004/auth-user",
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json", // JSON 형식임을 명시
+                Authorization: `Bearer ${userToken}`,
+              },
+            }
+          );
+          if (!response.ok) {
+            throw new Error("네트워크 응답이 정상적이지 않습니다.");
+          }
+          const data: User = await response.json();
+          setLoginUser(data);
+        } catch (error) {
+          console.error("로그인 유저 데이터 가져오기 실패: ", error);
+        }
+      }
+    };
+    getUser();
+  }, [userToken]);
+
+  // 로그아웃 버튼 클릭 시 로그아웃 API 요청
   const onClickSignout = async () => {
     try {
       const response = await fetch("https://kdt.frontend.5th.programmers.co.kr:5004/logout", {
         method: "POST",
         headers: {
           "Content-Type": "application/json", // JSON 형식임을 명시
-          Authorization: `bearer ${localStorageToken}`,
+          Authorization: `Bearer ${userToken}`,
         },
       });
-      // if (response.ok) {
-      //   window.localStorage.removeItem(localStorageToken); // 로컬 스토리지 내에 저장된 토큰을 삭제한다. - 추후 수정 예정
-      //   alert("로그아웃 되었습니다");
-      // }
-      console.log(response);
+      if (response.ok) {
+        window.localStorage.removeItem("userToken"); // 로컬 스토리지 내에 저장된 토큰을 삭제한다. - 추후 수정 예정
+        setIsLogin(false);
+        alert("로그아웃 되었습니다");
+      }
     } catch (error) {
       console.error("로그아웃 실패:", error);
     }
   };
+
   return (
     <HeaderBackground>
       <LogoImg onClick={() => navigate("/")} src={devlinkLogo} alt="devlink-logo" />
-      {islogin ? (
+      {isLogin ? (
         <HeaderButtons>
           <label
             onClick={() => {
-              setIslogin(!islogin);
               onClickSignout();
             }}>
             <img src={signoutIcon} alt="signout-icon" id="signout" /> <div>로그아웃</div>
           </label>
           <label
             onClick={() => {
-              navigate(`/profile/${username}`);
+              navigate(`/profile/${loginUser?._id}`);
             }}>
             <img src={userIcon} alt="user-icon" />
-            <div>{username}</div>
+            <div>{loginUser?.fullName}</div>
           </label>
         </HeaderButtons>
       ) : (
