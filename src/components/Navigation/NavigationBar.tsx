2afe187f8f03e361b1178404a36a228d379d5551
gyu <qkr9748200@gmail.com>
2024-10-05 17:15:44 +0900

    📝 Update: 충돌문제해결

--- src/components/Navigation/NavigationBar.tsx
+++ src/components/Navigation/NavigationBar.tsx
@@ -12,22 +12,17 @@ import { useEffect, useState } from "react";
 import Alarm from "../../features/Navigations/Alarm";
 import Notification from "../../features/Navigations/Notification";
 import Postsearch from "../../features/Navigations/Postsearch";
-import { useTheme } from "../../theme/ThemeContext";
 
-const Navcontainer = styled.div<{ darkMode: boolean }>`
+const Navcontainer = styled.div`
   border-right: 1.5px solid black;
   align-items: center;
   float: left;
-  /* darkMode 값에 따른 색상 조절 */
-  background-color: ${({ darkMode }) => (darkMode ? "#44474e" : "#F9F9F9")};
-  color: ${({ darkMode }) => (darkMode ? "white" : "black")};
+  background-color: #fff;
   width: 200px;
-  height: 100%;
-  //이거 어찌 해결해야 하나.. 트랜지션..
+  height: 100vh;
   overflow: hidden;
-  transition:
-    background-color 0.5s,
-    color 0.5s;
+  white-space: nowrap;
+  transition: 1s;
 
   &.closeNav {
     width: 105px;
@@ -38,13 +33,10 @@ const Navcontainer = styled.div<{ darkMode: boolean }>`
   }
 `;
 
-const Button = styled.button<{ darkMode: boolean }>`
+const Button = styled.button`
   display: flex;
   border: 0;
-  /* darkMode 값에 따른 색상 조절 */
-  background-color: ${({ darkMode }) => (darkMode ? "#44474e" : "#F9F9F9")};
-  color: ${({ darkMode }) => (darkMode ? "white" : "black")};
-
+  background-color: #fff;
   margin: 50px 30px;
   font-size: 15px;
   cursor: pointer;
@@ -58,9 +50,6 @@ const Button = styled.button<{ darkMode: boolean }>`
     width: 25px;
     height: 25px;
     margin-right: 16px;
-    /* darkMode 값에 따른 밝기 조절 */
-    filter: ${({ darkMode }) => (darkMode ? "brightness(1.75)" : "brightness(1)")};
-    transition: filter 0.5s;
   }
   span {
     margin-top: 5px;
@@ -77,6 +66,11 @@ const Button = styled.button<{ darkMode: boolean }>`
   }
 `;
 
+interface Profile {
+  _id: string;
+  user: string;
+}
+
 const NavigationBar = () => {
   const navigate = useNavigate();
   //네이게이션바 열고 닫기
@@ -84,25 +78,38 @@ const NavigationBar = () => {
   //알람과 포스트검색 패널 열고 닫기
   const [notiOpen, setNotiOpen] = useState(false);
   const [postOpen, setPostOpen] = useState(false);
-  //seen값
-  const [isSeen, setisSeen] = useState(false);
+  const [profile, setProfile] = useState<Profile>();
 
-  ////////////////////////////////////////////
-  const token =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7Il9pZCI6IjY2ZmE0ZWQ0ZDQ3NWE4N2RlMGFlMWE1NSIsImVtYWlsIjoidGVzdDA0QGFhYS5jb20ifSwiaWF0IjoxNzI3NjgwMzEzfQ.7rI5mmvcEa1wvVG2Qb2xhIz2ohiaC2XYwtakrMPHgLQ";
-  // 라벨 누르면 seen값  true로 바꾸는 fetch
+  const token = localStorage.getItem("userToken");
   useEffect(() => {
-    fetch("https://kdt.frontend.5th.programmers.co.kr:5004/notifications/seen", {
-      method: "PUT",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => setisSeen(data))
-      .catch((error) => console.error("Error:", error));
-  }, []);
+    const fetchData = async () => {
+      if (!token) {
+        console.error("No token found");
+        return;
+      }
+
+      try {
+        const response = await fetch("https://kdt.frontend.5th.programmers.co.kr:5004/auth-user", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch user profile");
+        }
+
+        const data: Profile = await response.json();
+        setProfile(data);
+      } catch (error) {
+        console.error("Error:", error);
+      }
+    };
+
+    fetchData();
+  }, [token]);
 
   const notiHandler = () => {
     if (notiOpen) {
@@ -122,63 +129,49 @@ const NavigationBar = () => {
     }
   };
 
-  // useTheme에서 themeToggle 함수와 state 가져오기
-  const { darkMode, themeToggle } = useTheme();
-
   return (
     <>
       {/* 알람, 포스트검색 패널 오픈 했을때 위치조정값 */}
       {notiOpen && <Notification isClosed={!showNav} />}
       {postOpen && <Postsearch postClosed={!showNav} />}
 
-      {/* Navcontainer, 모든 Button darkMode 값 전달 */}
-      <Navcontainer darkMode={darkMode} className={showNav ? "" : "closeNav"}>
-        <Button
-          onClick={() => navigate("/")}
-          darkMode={darkMode}
-          className={showNav ? "" : "btncloseNav"}>
+      <Navcontainer className={showNav ? "" : "closeNav"}>
+        <Button onClick={() => navigate("/")} className={showNav ? "" : "btncloseNav"}>
           <img src={home} alt="home" />
           <span>홈페이지</span>
         </Button>
 
-        <Button
-          onClick={() => navigate("/post/create")}
-          darkMode={darkMode}
-          className={showNav ? "" : "btncloseNav"}>
+        <Button onClick={() => navigate("/post/create")} className={showNav ? "" : "btncloseNav"}>
           <img src={postgeneration} alt="postgeneration" />
           <span>포스트 생성</span>
         </Button>
 
-        <Button onClick={postHandler} darkMode={darkMode} className={showNav ? "" : "btncloseNav"}>
+        <Button onClick={postHandler} className={showNav ? "" : "btncloseNav"}>
           <img src={postsearch} alt="postsearch" />
           <span>포스트 검색</span>
         </Button>
 
-        <Button onClick={notiHandler} darkMode={darkMode} className={showNav ? "" : "btncloseNav"}>
-          <Alarm isClosed={!showNav} isSeen={isSeen}>
+        <Button onClick={notiHandler} className={showNav ? "" : "btncloseNav"}>
+          <Alarm isClosed={!showNav}>
             <img src={alarm} alt="alarm" />
           </Alarm>
           <span>알림</span>
         </Button>
-        {/* 다크모드 버튼 - 클릭시 themeToggle 함수 실행 */}
-        <Button onClick={themeToggle} darkMode={darkMode} className={showNav ? "" : "btncloseNav"}>
+
+        <Button className={showNav ? "" : "btncloseNav"}>
           <img src={darkmode} alt="darkmode" />
           <span>다크 모드</span>
         </Button>
 
         {/* 유저아이디로 다시 지정해야함 */}
         <Button
-          onClick={() => navigate("/profile/1")}
-          darkMode={darkMode}
+          onClick={() => profile && navigate(`/profile/${profile._id}`)}
           className={showNav ? "" : "btncloseNav"}>
           <img src={mypage} alt="mypage" />
           <span>마이페이지</span>
         </Button>
 
-        <Button
-          onClick={() => setShowNav(!showNav)}
-          darkMode={darkMode}
-          className={showNav ? "" : "btncloseNav"}>
+        <Button onClick={() => setShowNav(!showNav)} className={showNav ? "" : "btncloseNav"}>
           <img src={showNav ? reduction : expantion} alt="reduction" />
           <span>축소</span>
         </Button>
